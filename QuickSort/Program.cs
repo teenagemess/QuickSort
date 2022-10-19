b42003f6e452095b24938b1b319b16062b4ec47c
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-10-19 16:58:05 +0700

    menambahkan looping dan percabangan

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -16,8 +16,18 @@ namespace QuickSort
         {
             while(true)
             {
-
+                while(true){
+                    Console.Write("Enter the number of elements in the array :");
+                    string s = Console.ReadLine();
+                    n = Int32.Parse(s);
+                    if (n <= 20)
+                        break;
+                    else
+                        Console.WriteLine("\n Array can have maximum 20 elements \n");
             }
+                Console.WriteLine("\n==================");
+                Console.WriteLine("Enter Array Elements");
+                Console.WriteLine("\n==================");
         }
     }
 }
