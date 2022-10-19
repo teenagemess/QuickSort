8bda2850552fb127fcb7c27e5100ea43e84a3067
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-10-19 16:44:52 +0700

    add array of integers to hold values, number of comparasion and number of data movements

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -3,4 +3,10 @@
 namespace QuickSort
 {
     class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; //number of data movements
+    }
 }
