3a7fdf88f8bf1e6cf7f9a23e52c0e5ee099ef3a4
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-10-19 18:10:43 +0700

    memperbaiki console.WriteLine di bagian get array elements

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -36,7 +36,7 @@ namespace QuickSort
             //get array elements
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("<" + (i = 1) + ">");
+                Console.WriteLine("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
 
